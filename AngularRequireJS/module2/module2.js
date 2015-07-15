fe4ff0afe366fce1a8fc5ecd176978a8e5870295
kenkozheng <kenkofox@qq.com>
2015-07-15 10:01:57 +0800

    增加不同注入写法

--- AngularRequireJS/module2/module2.js
+++ AngularRequireJS/module2/module2.js
@@ -10,4 +10,11 @@ define(['angular', 'text!module2/tpl.html'], function (angular, tpl) {
         },
         tpl: tpl
     };
+
+    //也可以使用这样的显式注入方式，angular执行controller函数前，会先读取$inject
+    controller.$inject = ['$scope'];
+    function controller(s){
+        s.date = '2015-07-13';
+    }
+    return {controller:controller, tpl:tpl};
 });
