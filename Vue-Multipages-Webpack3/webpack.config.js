e91ea7d5a0455292b493ba7820726d305eb8df5f
kenkozheng <160150553@qq.com>
2017-11-27 00:57:43 +0800

    Vue-Multipages-Webpack3
    初稿

---
+++ Vue-Multipages-Webpack3/webpack.config.js
@@ -0,0 +1,133 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var WriteFilePlugin = require('write-file-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+//kenko 修改点
+var pages = ['page1', 'page2'];
+var entry = {};
+pages.forEach(function (pageName) {
+    entry[pageName] = `./src/pages/${pageName}/main.js`;
+});
+var dist = process.env.NODE_ENV === 'production' ? 'dist' : 'dev';      //开发中和发布后放置不同目录
+//////////////
+
+module.exports = {
+    entry: entry,
+    output: {
+        path: path.resolve(__dirname, `./${dist}/`),
+        publicPath: '/',       //发布后在线访问的url。dev模式下，使用的是express在当前项目根目录启动
+        filename: `[name].js`   //'[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            }, {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: './img/[name].[hash].[ext]'    //自动hash命名图片等资源，并修改路径。路径需要根据项目实际情况确定
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map',
+
+    //kenko 修改点。把css目录的按相对路径copy过去。默认情况下，3.0版本不写文件，所以devServer下无效 https://www.npmjs.com/package/copy-webpack-plugin
+    //配合WriteFilePlugin可以强制写入。使用HtmlWebpackPlugin后，在devserver也不会生成文件，也需要强制
+    //不使用devserver访问，就更需要强制写入了。例如fiddler替换
+    plugins: [
+        new CleanWebpackPlugin([dist]),
+        new CopyWebpackPlugin([
+            {context: 'src/css/', from: '**/*', to: path.resolve(__dirname, `${dist}/css`)}
+        ]),
+        new WriteFilePlugin({
+            test: /\.css|\.html|\.js$/,     // Write only files that match the regexp
+            useHashIndex: true  //Use hash index to write only files that have changed since the last iteration
+        })
+    ]
+}
+
+//kenko 修改点
+pages.forEach(function (pageName) {
+    module.exports.plugins.push(
+        new HtmlWebpackPlugin({
+            filename: `${pageName}.html`,
+            template: `./src/pages/${pageName}/main.html`,
+            chunks: [pageName],
+            inlineSource: '.(js|css)$' // embed all javascript and css inline。结合HtmlWebpackInlineSourcePlugin才有效果
+        })
+    );
+});
+//////////////
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            //sourceMap: true,  //开启max_line_len后会有报错，二选一
+            compress: {
+                warnings: false
+            },
+            output: {
+                max_line_len: 100
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        //new HtmlWebpackPlugin({
+        //    filename: `${pageName}.html`,
+        //    template: `./src/pages/${pageName}/main.html`,
+        //    inlineSource: '.(js|css)$' // embed all javascript and css inline
+        //}),
+        //new HtmlWebpackInlineSourcePlugin() //内联css、js
+    ]);
+
+    //kenko 修改点
+    module.exports.plugins.push(
+        new HtmlWebpackInlineSourcePlugin() //内联css、js。配合HtmlWebpackPlugin
+    );
+    //////////////
+}
