489c397931ba4ee98c49435ff05cf2c2c95a1891
Kenko <160150553@qq.com>
2018-04-09 12:04:59 +0800

    添加PWA demo

---
+++ PWA/sw.js
@@ -0,0 +1,54 @@
+/**
+ * Created by kenkozheng on 2018/4/9.
+ */
+
+// 监听 service worker 的 install 事件
+this.addEventListener('install', function (event) {
+    // 如果监听到了 service worker 已经安装成功的话，就会调用 event.waitUntil 回调函数
+    event.waitUntil(
+        // 安装成功后操作 CacheStorage 缓存，使用之前需要先通过 caches.open() 打开对应缓存空间。
+        caches.open('my-test-cache-v1').then(function (cache) {
+            // 通过 cache 缓存对象的 addAll 方法添加 precache 缓存
+            return cache.addAll([
+                'index.html',
+                'index.jpg'
+            ]);
+        })
+    );
+});
+
+this.addEventListener('fetch', function (event) {
+    console.log('fetch', event.request);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // 来来来，代理可以搞一些代理的事情
+
+            // 如果 Service Worker 有自己的返回，就直接返回，减少一次 http 请求
+            if (response) {
+                return response;
+            }
+
+            console.log('no cache', event.request);
+
+            // 如果 service worker 没有返回，那就得直接请求真实远程服务
+            var request = event.request.clone(); // 把原始请求拷过来
+            return fetch(request).then(function (httpRes) {
+
+                // http请求的返回已被抓到，可以处置了。
+
+                // 请求失败了，直接返回失败的结果就好了。。
+                if (!httpRes || httpRes.status !== 200) {
+                    return httpRes;
+                }
+
+                // 请求成功的话，将请求缓存起来。
+                var responseClone = httpRes.clone();
+                caches.open('my-test-cache-v1').then(function (cache) {
+                    cache.put(event.request, responseClone);
+                });
+
+                return httpRes;
+            });
+        })
+    );
+});
